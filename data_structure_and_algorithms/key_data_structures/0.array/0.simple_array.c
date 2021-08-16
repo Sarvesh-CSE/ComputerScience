f3d742bba1be404c3f0b49d52079ac55d42d9eda
Sarvesh <pandeysarvesh100@gmail.com>
2021-08-16 08:40:20 +0530

    array concept

---
+++ data_structure_and_algorithms/key_data_structures/0.array/0.simple_array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+# include<conio.h>
+void main()
+{
+    int my_first_array_variable[5] = {1, 2, 3, 4, 5};
+    printf("Which element do you want to fetch? \n");
+    printf("Can I get one at index 4?  \n")
+    printf("Element at %d index is %d", 4, my_first_array_variable[4] );
+}
