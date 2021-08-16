8ee47f4470d5cf2a1792baa4ffaad0290ec7970d
Sarvesh <pandeysarvesh100@gmail.com>
2021-08-16 10:44:52 +0530

    changed the return type of main

--- data_structures_and_algorithms/key_data_structures/0.array/0.simple_array.c
+++ data_structures_and_algorithms/key_data_structures/0.array/0.simple_array.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 # include<conio.h>
-void main()
+int main()
 {
     int my_first_array_variable[5] = {1, 2, 3, 4, 5};
     printf("Which element do you want to fetch? \n");
     printf("Can I get one at index 4?  \n")
     printf("Element at %d index is %d", 4, my_first_array_variable[4] );
+    return 0;
 }
