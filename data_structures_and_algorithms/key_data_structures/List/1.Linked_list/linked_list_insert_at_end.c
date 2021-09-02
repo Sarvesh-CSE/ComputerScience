e5c6fcca3473c6fb1d891e3a723daa62c7adad96
Sarvesh <pandeysarvesh100@gmail.com>
2021-09-02 10:01:22 +0530

    rearranged code to reduce line count

---
+++ data_structures_and_algorithms/key_data_structures/List/1.Linked_list/linked_list_insert_at_end.c
@@ -0,0 +1,34 @@
+// Program for inserting an element at the end of Linked List
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node   
+{  
+    int data;  
+    struct node *next;   
+};  
+struct node *head;
+
+void main()
+{
+    struct node *temp, *new_node;  
+    int item;     
+    new_node = (struct node*)malloc(sizeof(struct node)); 
+ 
+    printf("\nEnter value?\n");  
+    scanf("%d",&item);  
+    new_node->data = item;  
+    new_node -> next = NULL;  
+    if(head == NULL)  
+        head = new_node;  
+    else  
+    {  
+        temp = head;  
+        while (temp -> next != NULL){ 
+            temp = temp -> next; } 
+        temp->next = new_node;  
+    }
+        printf("\n Node inserted with value-- %d", new_node->data);
+  
+}  
