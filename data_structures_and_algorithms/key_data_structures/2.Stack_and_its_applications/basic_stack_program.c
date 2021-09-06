ee1dcfb7eca3c6a8e3c60a9a37862ef6658caa47
Sarvesh <pandeysarvesh100@gmail.com>
2021-09-06 09:49:24 +0530

    basic stack program

--- data_structures_and_algorithms/key_data_structures/2.Stack_and_its_applications/basic_stack_program.c
+++ data_structures_and_algorithms/key_data_structures/2.Stack_and_its_applications/basic_stack_program.c
@@ -1,10 +1,13 @@
+// Implementation of stack data structure using array data structure.
+
 #include <stdio.h>
 
-int MAXSIZE = 8;       
-int stack[8];     
+# define MAXSIZE 8
+
+int stack_arr[MAXSIZE];     
 int top = -1;            
 
-int isempty() // to check whether stack is empty or not
+int isempty() // to check whether stack_arr is empty or not
 {
     if(top == -1)
       return 1;
@@ -12,17 +15,17 @@ int isempty() // to check whether stack is empty or not
       return 0;
 }
    
-int isfull() // to check whether stack is full or not
+int isfull() // to check whether stack_arr is full or not
 {
-    if(top == MAXSIZE)
+    if(top >= MAXSIZE)
       return 1;
     else
       return 0;
 }
 
-int peek() // to get the element present on top of the stack.
+int peek() // to get the element present on top of the stack_arr.
 {
-   return stack[top];
+   return stack_arr[top];
 }
 
 int pop() // to remove element from top
@@ -31,11 +34,11 @@ int pop() // to remove element from top
 	
    if(!isempty()) 
    {
-    data = stack[top];
+    data = stack_arr[top];
     top = top - 1;   
     return data;
    } 
-   else printf("Could not retrieve data, Stack is empty.\n");
+   else printf("Could not retrieve data, stack_arr is empty.\n");
 }
 
 int push(int data) // function to insert element at top
@@ -43,13 +46,14 @@ int push(int data) // function to insert element at top
 
    if(!isfull()) {
       top = top + 1;   
-      stack[top] = data;
-   } else printf("Could not insert data, Stack is full.\n");
+      stack_arr[top] = data;
+      printf("The element pushed is %d \n", stack_arr[top]);
+   } else printf("Could not insert data, stack_arr is full.\n");
 }
 
 int main() 
 {
-   // push items on to the stack 
+   // push items on to the stack_arr 
    push(3);
    push(5);
    push(9);
@@ -57,17 +61,22 @@ int main()
    push(12);
    push(15);
 
-   printf("Element at top of the stack: %d\n" ,peek());
+   printf(" ----------------------------------------------\n");
+   printf("Element at top of the stack_arr: %d\n" ,peek());
+   printf(" ----------------------------------------------\n");
+
+   printf("stack_arr empty: %s\n" , isempty()?"true":"false");
+   printf("stack_arr full: %s\n" , isfull()?"true":"false");
    printf("Elements: \n");
 
-   // print stack data 
+   // print stack_arr data 
    while(!isempty()) {
       int data = pop();
       printf("%d\n",data);
    }
+   printf("stack_arr empty: %s\n" , isempty()?"true":"false");
+   printf("stack_arr full: %s\n" , isfull()?"true":"false");
 
-   printf("Stack full: %s\n" , isfull()?"true":"false");
-   printf("Stack empty: %s\n" , isempty()?"true":"false");
    
    return 0;
 }
