9edcc70881918b6644d57b0d6f32dd54165bef37
Sarvesh <pandeysarvesh100@gmail.com>
2021-09-15 10:08:39 +0530

    tranversal of binary tree

--- data_structures_and_algorithms/key_data_structures/Non_linear_data_structures/Tree/tree_creation.c
+++ data_structures_and_algorithms/key_data_structures/Non_linear_data_structures/Tree/tree_creation.c
@@ -1,21 +1,15 @@
-// C program for different tree traversals
 #include <stdio.h>
 #include <stdlib.h>
 
-/* A binary tree node has data, pointer to left child
-and a pointer to right child */
 struct node {
 	int data;
 	struct node* left;
 	struct node* right;
 };
 
-/* function that allocates a new node with the
-given data and NULL left and right pointers. */
-struct node* newNode(int data)
+struct node* new_node(int data)
 {
-	struct node* node
-		= (struct node*)malloc(sizeof(struct node));
+	struct node* node = (struct node*)malloc(sizeof(struct node));
 	node->data = data;
 	node->left = NULL;
 	node->right = NULL;
@@ -23,12 +17,55 @@ struct node* newNode(int data)
 	return (node);
 }
 
+//Print nodes of a given binary tree using postorder traversal
+void preorder_traversal(struct node* node)
+{
+    if (node != NULL)
+    {
+        printf("%d ", node->data);
+        preorder_traversal(node->left);
+        preorder_traversal(node->right);
+    }
+}
+
+//Print nodes of a given binary tree using inorder traversal
+void inorder_traversal(struct node* node)
+{
+    if (node != NULL)
+    {
+        inorder_traversal(node->left);
+        printf("%d ", node->data);
+        inorder_traversal(node->right);
+    }
+}
+
+//Print nodes of a given binary tree using postorder traversal
+void postorder_traversal(struct node* node)
+{
+    if (node != NULL)
+    {
+        postorder_traversal(node->left);
+        postorder_traversal(node->right);
+        printf("%d ", node->data);
+    }
+}
+
 int main()
 {
-	struct node* root = newNode(1);
-	root->left = newNode(2);
-	root->right = newNode(3);
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
+	struct node* root = new_node(1);
+	root->left = new_node(2);
+	root->right = new_node(3);
+	root->left->left = new_node(4);
+	root->left->right = new_node(5);
+
+    printf("\n Preorder traversal of binary tree is \n");
+    preorder_traversal(root);
+ 
+    printf("\n Inorder traversal of binary tree is \n");
+    inorder_traversal(root);
+ 
+    printf("\n Postorder traversal of binary tree is \n");
+    postorder_traversal(root);
+
 	return 0;
 }
